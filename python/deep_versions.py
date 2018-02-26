c9406057822ae0086951679da4f84e2af16a6882
chakrim99 <chakri.m99@gmail.com>
2018-02-26 22:58:14 +0530

    Add files via upload

---
+++ python/deep_versions.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 23 23:13:00 2018
+
+@author: chakradhar
+"""
+
+# theano
+import theano
+print('theano: %s' % theano.__version__)
+# tensorflow
+import tensorflow
+print('tensorflow: %s' % tensorflow.__version__)
+# keras
+import keras
+print('keras: %s' % keras.__version__)
